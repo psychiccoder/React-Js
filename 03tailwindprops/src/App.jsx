4a80fe576cf75c967f745a2aee71515d67dc4708
Pushkar Tiwari <ptpushkartiwari@gmail.com>
2024-04-19 16:14:02 +0530

    change few files

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -13,9 +13,9 @@ function App() {
   return (
     <>
       <h1 className='bg-yellow-300 text-black p-5 rounded-xl mb-4'>Tailwind test</h1>
-      {/* <Card type="Read" someObj={myobj} someArr={newArr} />
+      <Card type="Read" someObj={myobj} someArr={newArr} />
       <Card type="Study" />
-      <Card type="Learn" /> */}
+      <Card type="Learn" />
       <SignIn name="Twitter" name2="Instagram" start="Get started" />
       <SignIn name="Reddit" name2="Facebook" start="Let's start" />
       <SignIn name="Google" name2="Canva" start="Chaliye Shuru Karte Hain" />
