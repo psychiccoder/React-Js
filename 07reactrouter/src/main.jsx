b5b0bb3c452ec5fb12186b6447dbdb321b8030ec
Pushkar Tiwari <ptpushkartiwari@gmail.com>
2024-04-19 15:35:42 +0530

    change

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
-      <Route path='signin' element={<SignIn/>} />
+      <Route path='signin' element={<SignIn />} />
       <Route path='user/:userid' element={<User />} />
       <Route
         loader={githubInfoLoader}
