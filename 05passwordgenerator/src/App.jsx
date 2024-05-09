e19ada962051050c5e53b61a504494d82392bf0f
Pushkar Tiwari <ptpushkartiwari@gmail.com>
2024-05-09 15:13:29 +0530

    deleted unused projects

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -40,6 +40,7 @@ function App() {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
 
+  // passwordGenerator();
   return (
 
     <div className=" w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-blue-500 text-slate-950">
